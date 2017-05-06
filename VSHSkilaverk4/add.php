627bf07b5737a0c4d41a9714251776011752ce9d
andrisnrb <andri.brynjarsson@gmail.com>
2017-05-06 21:50:11 +0000

    Hérna er rétta fileið

---
+++ VSHSkilaverk4/add.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE HTML>
+<html>
+	<head>	
+		<link rel="stylesheet" href="snidmatII.css">
+		<title>Bæta við</title> 
+	</head>
+  	<body>
+  		<h3>Innskráningarform:</h3>
+		<form action="database.php" method="post">
+		    <p>
+		        <label>Nafn:</label>
+		        <input type="text" name="Nafn">
+		    </p>
+		    <p>
+		        <label>Aldur:</label>
+		        <input type="text" name="Aldur">
+		    </p>
+		    <input type="submit" value="Skrá">
+		</form>
+  	</body> 
+</html>
+
+
+
+
+
+
+
